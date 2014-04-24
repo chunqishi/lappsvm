0ed56aaf5199e4bf095e52b61ed97947a67d5d1f
chunqishi <shicq@cs.brandeis.edu>
2014-04-24 19:05:03 -0400

    added status

--- bin/package.sh
+++ bin/package.sh
@@ -2,4 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-zip ${DIR}/../lappsvm-install.zip ${DIR}/../src/*.sh
+cd ${DIR}/../src/
+zip ${DIR}/../lappsvm-install.zip *.sh
+cd ${DIR}
